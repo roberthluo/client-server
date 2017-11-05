9be5722fdf1f3e150d96407988666cc9a679a6c9
robert <robert.otherone@gmail.com>
2017-11-05 13:01:48 -0500

    Added server to continue when it recieves a bye

--- myClient.java
+++ myClient.java
@@ -2,16 +2,12 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
-
 public class myClient
 {
-  Socket client;
-  ObjectOutputStream out;
-  ObjectInputStream in;
-  String message;
+  Socket socket;
   public String ipAddress = "";
   public int port = 0;
-
+  BufferedWriter bw;
 
   public myClient(String ipAddress, int port)
   {
@@ -19,10 +15,14 @@ public class myClient
     this.port = port;
 
     try{
-      client = new Socket("localhost", port);
-      out = new ObjectOutputStream(client.getOutputStream());
-      out.flush();
-      in = new ObjectInputStream(client.getInputStream());
+      //Send the message to the server
+      socket = new Socket(ipAddress, port);
+      OutputStream os = socket.getOutputStream();
+      OutputStreamWriter osw = new OutputStreamWriter(os);
+      bw = new BufferedWriter(osw);
+
+      System.out.println("Session has been established.");
+
     }
     catch(UnknownHostException unknownHost){
       System.err.println("You are trying to connect to an unknown host!");
@@ -30,45 +30,56 @@ public class myClient
     catch(IOException ioException){
       ioException.printStackTrace();
     }
-    System.out.println("Session has been established.");
+
   }
 
   public void getIPAddress()
   {
-    System.out.println("server:"+ client.getRemoteSocketAddress());
+    System.out.println("server:"+ socket.getRemoteSocketAddress());
   }
 
   public void startEcho()
   {
-
     System.out.println("listening");
     Scanner reader = new Scanner(System.in);
+
     while(true){
       String command = reader.next();
       if(command.equals("stopEcho")){
         break;
       }
-      sendMessage(command);
+
+      String sendMessage = command + "\n";
+
+      //Get the return message from the server
+      try
+      {
+        bw.write(sendMessage);
+        bw.flush();
+        OutputStream os = socket.getOutputStream();
+        OutputStreamWriter osw = new OutputStreamWriter(os);
+        bw = new BufferedWriter(osw);
+        System.out.println("Message sent to the server : "+sendMessage);
+        InputStream is = socket.getInputStream();
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(isr);
+        String message = br.readLine();
+        System.out.println("Message received from the server : " +message + "\n");
+      }
+      catch (Exception exception)
+      {
+        exception.printStackTrace();
+      }
+
     }
     System.out.println("stopped");
   }
 
-  void sendMessage(String msg)
-  {
-      try{
-          out.writeObject(msg);
-          out.flush();
-          System.out.println("echo>" + msg);
-      }
-      catch(IOException ioException){
-          ioException.printStackTrace();
-      }
-  }
 
   public void disconnect()
   {
     try{
-      client.close();
+      socket.close();
     }
     catch(IOException ioException){
         ioException.printStackTrace();
@@ -102,6 +113,4 @@ public class myClient
     reader.close();
     System.out.println("End of client session.");
   }
-
-
 }
--- myServer.java
+++ myServer.java
@@ -8,82 +8,66 @@ public class myServer
   String message = "";
   File file = new File("log.txt");
   public static PrintWriter writer;
-
-  public myServer()
-  {
-    try
-    {
-      writer = new PrintWriter("log.txt", "UTF-8");
-      writer.println("Start of log:");
-    }
-    catch (IOException e){
-      e.printStackTrace();
-    }
-  }
-
-  public void run(int portNumber)
-  {
-    while(true){
-      try{
-        ServerSocket requestSocket = new ServerSocket(portNumber);
-        Socket connection = requestSocket.accept();
-
-        // Needs to accept connection before continuing
-        System.out.println("Session has been established.");
-        in = new ObjectInputStream(connection.getInputStream());
-        out = new ObjectOutputStream(connection.getOutputStream());
-        out.flush();
-
-        do{
-          try{
-            message = (String)in.readObject();
-            writer.println("client>" + message);
-            sendMessage(message);
-
-          }
-          catch(Exception e){
-            System.err.println(e);
-          }
-
-        }while(!message.equals("bye"));
-      }
-      catch(Exception e){
-        System.out.println(e);
-      }
-      //closing writer
-      writer.println("End of log.");
-      writer.close();
-    }
-
-  }
-
-  public void sendMessage(String msg)
-  {
-    try{
-      out.writeObject(msg);
-      out.flush();
-      writer.println("server>" + msg);
-    }
-    catch(IOException ioException){
-      ioException.printStackTrace();
-    }
-  }
+  ServerSocket requestSocket;
+  private static Socket socket;
 
   public static void main(String args[])
   {
-    myServer server = new myServer();
-    System.out.println("New Server port:" + args[0]);
-    int port = Integer.parseInt(args[0]);
-    server.run(port);
-    Runtime.getRuntime().addShutdownHook(new Thread()
+    try
     {
-      public void run()
-      {
-        writer.close();
-        System.out.println("In shutdown hook");
-      }
-    });
-    System.out.println("Application Terminating ...");
 
+      int port = Integer.parseInt(args[0]);
+      ServerSocket serverSocket = new ServerSocket(port);
+      socket = serverSocket.accept();
+      System.out.println("Server Started and listening to the port "+ port);
+
+      //Server is running always. This is done using this while(true) loop
+      while(true)
+      {
+        //Reading the message from the client
+        InputStream is = socket.getInputStream();
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(isr);
+        String message = br.readLine();
+
+        if(message == null){
+          socket = serverSocket.accept();
+        }
+        System.out.println("Message received from client is "+message);
+
+        String returnMessage;
+        try
+        {
+            returnMessage = "server:" + message + "\n";
+        }
+        catch(Exception e)
+        {
+            //Input was not a number. Sending proper message back to client.
+            returnMessage = "Please send a proper message\n";
+        }
+
+        //Sending the response back to the client.
+        OutputStream os = socket.getOutputStream();
+        OutputStreamWriter osw = new OutputStreamWriter(os);
+        BufferedWriter bw = new BufferedWriter(osw);
+        bw.write(returnMessage);
+        System.out.println("Message sent to the client is "+returnMessage);
+        bw.flush();
+      }
+    }
+    catch (Exception e)
+    {
+      e.printStackTrace();
+    }
+    // finally
+    // {
+    //   try
+    //   {
+    //     socket.close();
+    //   }
+    //   catch(Exception e){}
+    //   }
+    // }
   }
+
 }
